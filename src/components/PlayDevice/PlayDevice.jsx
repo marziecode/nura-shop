ea047dcf49896ba24dd4337a97e091fc31e89980
marziecode <marzieh2802@gmail.com>
2024-03-06 18:23:30 +0330

    change the ui component playDevice

--- src/components/PlayDevice/PlayDevice.jsx
+++ src/components/PlayDevice/PlayDevice.jsx
@@ -3,7 +3,6 @@ import playDeviceIMG from '../../assets/playDevice-img.svg'
 function PlayDevice() {
     return <div className='contant-container-playDevice'>
         <div className='container-mask-img-playDevice'>
-            <img src={playDeviceIMG} alt='playDevice-img' />
             <div className='text-container-playDevice'>
                 <h1 className='lable-text-container-playDevice'>Play from <br />any device.</h1>
                 <p className='text-playDevice'>Your hearing profile is <span className='higlight-playDevice'>stored on the earbuds</span>, and is applied to whatever you listen to, <span className='higlight-playDevice'>on any device</span>.</p>
